9e29bb1f6fb6324934da13e565a25d4c9e377e28
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-09-12 14:28:23 +0300

    feat: table attribute for each database

--- Src/Gedaq.Provider/Attributes/TableAttribute.cs
+++ Src/Gedaq.Provider/Attributes/TableAttribute.cs
@@ -3,7 +3,7 @@
 namespace Gedaq.Provider.Attributes
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
-    public sealed class TableAttribute : Attribute
+    public abstract class TableAttribute : Attribute
     {
         public TableAttribute(string nameInDatabase)
         {
