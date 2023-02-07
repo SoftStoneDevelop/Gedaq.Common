2bc655bf3f8d5d9fb2b48348096fbe7e32850bdf
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-07 04:15:33 +0300

    feat: + generated method type

--- Src/Gedaq.Provider/Attributes/QueryAttribute.cs
+++ Src/Gedaq.Provider/Attributes/QueryAttribute.cs
@@ -6,9 +6,9 @@ namespace Gedaq.Provider.Attributes
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
     public abstract class QueryAttribute : Attribute
     {
-        private QueryAttribute() { }
+        protected QueryAttribute() { }
 
-        protected QueryAttribute((string query, string itemTypeName, ResultType resultType)[] queries)
+        protected QueryAttribute((string query, string itemTypeName, ResultType resultMethodType)[] queries, MethodType methodType)
         {
         }
     }
---
+++ Src/Gedaq.Provider/Enums/MethodType.cs
@@ -0,0 +1,8 @@
+﻿namespace Gedaq.Provider.Enums
+{
+    public enum MethodType
+    {
+        Sync = 0,
+        Async = 1
+    }
+}
