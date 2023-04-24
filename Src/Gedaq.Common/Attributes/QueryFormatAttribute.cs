39630914e9837a4f8f554d1dce05a7c24008614e
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-04-24 22:54:04 +0300

    feat: FormatAttribute

---
+++ Src/Gedaq.Common/Attributes/QueryFormatAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gedaq.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class QueryFormatAttribute : Attribute
+    {
+        public QueryFormatAttribute(
+            string methodName,
+            int position
+            )
+        {
+        }
+    }
+}
