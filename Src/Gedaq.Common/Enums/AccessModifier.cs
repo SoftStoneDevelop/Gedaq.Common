0a7020321a3b838789790b94df42564b3d4ed2c0
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-05-09 06:54:20 +0300

    feat: + add parameter access modifier for generate partial class(https://github.com/SoftStoneDevelop/Gedaq/issues/12)

---
+++ Src/Gedaq.Common/Enums/AccessModifier.cs
@@ -0,0 +1,11 @@
+﻿namespace Gedaq.Common.Enums
+{
+    public enum AccessModifier
+    {
+        AsContainingClass = 0,
+        Public = 1,
+        Private = 2,
+        Internal = 3,
+        Protected = 4,
+    }
+}
