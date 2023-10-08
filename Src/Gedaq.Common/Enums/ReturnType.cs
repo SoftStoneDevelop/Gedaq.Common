afe6ac10871ee3687e459d9328d6d99d53920ac4
softstonedevelop <softstonedevelop@gmail.com>
2023-10-08 16:54:19 +0300

    feat:
    + ReturnType enum
    
    https://github.com/SoftStoneDevelop/Gedaq/issues/53

---
+++ Src/Gedaq.Common/Enums/ReturnType.cs
@@ -0,0 +1,11 @@
+﻿namespace Gedaq.Common.Enums
+{
+    public enum ReturnType
+    {
+        Enumerable = 1,
+        Single = 2,
+        SingleOrDefault = 3,
+        First = 4,
+        FirstOrDefault = 5
+    }
+}
