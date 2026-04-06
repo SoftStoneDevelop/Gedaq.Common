42fade430b32fc41852b93e4303b3bad1a40a21b
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-04-06 23:03:11 +0300

    feat: + Attribute for dynamic query mapping
    #https://github.com/SoftStoneDevelop/Gedaq/issues/58

---
+++ Src/Gedaq.Common/Attributes/AliasAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gedaq.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class AliasAttribute : Attribute
+    {       
+        /// <param name="alias">Alias of column in sql</param>
+        /// <remarks>If the alias is an empty string, the name of the property on which it was placed will be used as the alias. Also remember about AliasPrefix attribute..</remarks>
+        public AliasAttribute(string alias = "")
+        {
+        }
+    }
+}
---
+++ Src/Gedaq.Common/Attributes/AliasPrefixAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gedaq.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class AliasPrefixAttribute : Attribute
+    {
+        /// <param name="prefix">prefix for the all aliases contained in class</param>
+        /// <remarks>Prefix and aliases used only if you choose to generate dynamic query. Prefix can be empty string only if query return single type.</remarks>
+        public AliasPrefixAttribute(string prefix = "")
+        {
+        }
+    }
+}
